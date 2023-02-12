7b6a2407cf09a277ae04877d2c5984d1418d25a2
Daniel Suarez-Mash <67163110+jollychappies@users.noreply.github.com>
2023-02-12 18:31:54 +0000

    Added about-me and ds-projects pages. Added selfie image with small bio. Add titanic summary and image.

--- flask_tool.py
+++ flask_tool.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
 people = {
-    'name': 'Daniel',
+    'name': 'Daniel Suarez-Mash',
     'age': 24,
     'address': 'Solihull'
 }
@@ -17,6 +17,18 @@ def home():
     return render_template('index.html', people=people, ghub=ghub)
 
 
+# About page
+@app.route("/about-me")
+def about_me():
+    return render_template('about-me.html', people=people, ghub=ghub)
+
+
+# About page
+@app.route("/ds-projects")
+def ds_projects():
+    return render_template('ds-projects.html', people=people, ghub=ghub)
+
+
 # Whatever I type after /home/{....} will be captured by the python function as an input
 @app.route('/<name>')
 def user(name):
@@ -27,6 +39,12 @@ def user(name):
 @app.route('/admin')
 def admin():
     return redirect(url_for('home'))
+
+
+# send static files from directory
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 
 if __name__ == '__main__':
